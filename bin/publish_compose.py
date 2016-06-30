d3a15a827bbc60ad0e363448e5e21ffc44709d74
Jonas Michel <jmichel@cognitivescale.com>
2016-06-30 16:28:46 -0500

    ENG-519: correctly update published compose file with gocd dependencies

--- bin/publish_compose.py
+++ bin/publish_compose.py
@@ -22,7 +22,16 @@ def merge_dict(original, update):
     return update
 
 def get_compose_services(compose):
-    return [ key for key in compose ]
+    services = {}
+    for key in compose:
+        if not compose[key].has_key('image'): continue
+        services[key] = get_image_parts(compose[key].get('image'))
+    return services
+
+def get_image_parts(image):
+    image_parts = image.split(':')
+    if len(image_parts) < 2: image_parts.append('latest')
+    return {'image': image_parts[0], 'tag': image_parts[1]}
 
 def get_version_env(services, images):
      return { service: { 'image': images[image] }  for image in images  for service in services
@@ -40,6 +49,13 @@ def get_pipeline_versions(url,artifact,username,password):
     timestamp = re.search("\"timestamp\":[ ]?\"(.*)\"",str).group(1)
     eprint ("Applying service: {} image: {} built_on: {}".format(name,image,timestamp))
     return {name: { 'image':image}}
+
+def filter_locators(locators, services):
+    service_images = [services[service]['image'] for service in services]
+    def match(locator):
+        locator_image = get_image_parts(locators[locator].get('image'))
+        return locator_image['image'] in service_images
+    return { locator: locators[locator] for locator in locators if match(locator) }
 
 def usage():
     eprint("Usage: -s|--source <source> -u|--user <username> -p|--password <password>")
@@ -87,7 +103,7 @@ def main(argv):
     locators={ k: v for d in [ get_pipeline_versions(url,os.environ.get(key),cduser,cdpass)
             for key in os.environ.keys()
                 if key.startswith("GO_DEPENDENCY_LOCATOR_")] for k, v in d.items() }
-    filtered_locators = { locator: locators[locator] for locator in locators if locator in services}
+    filtered_locators = filter_locators(locators, services)
     withversions=merge_dict(withstaticversions,filtered_locators)
     print(yaml.safe_dump(withversions,indent=2, default_flow_style=False))
 
