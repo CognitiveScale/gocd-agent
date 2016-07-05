0737176deb46b955fe8a55dbc8a57d75812bbd8a
Johan Gielstra <jgielstra@cognitivescale.com>
2016-07-05 13:47:25 -0500

    supress python error message

--- bin/publish_compose.py
+++ bin/publish_compose.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import requests, re, sys, json, os, yaml, getopt
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 def load_compose(file):
     with open(file, 'r') as stream:
